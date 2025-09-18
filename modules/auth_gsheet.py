25beb0f51d5ae7634f76feba3ff55c40b2402e47
suthataccworks <suthat.accworks@gmail.com>
2025-09-18 10:14:21 +0700

    Create auth_gsheet.py

---
+++ modules/auth_gsheet.py
@@ -0,0 +1,10 @@
+import gspread
+import streamlit as st
+from oauth2client.service_account import ServiceAccountCredentials
+
+def get_sheet():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+    client = gspread.authorize(creds)
+    sheet = client.open("UserManagement").worksheet("users")
+    return sheet
