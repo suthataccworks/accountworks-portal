8474bdda629dee4d8fd4bf6208928f06048032c2
AM accountworks <suthat.accworks@gmail.com>
2025-09-23 09:36:15 +0700

    chore(migrations): backfill LeaveBalance nulls to 0

---
+++ hr/migrations/0010_backfill_leavebalance_nulls.py
@@ -0,0 +1,26 @@
+from django.db import migrations
+
+def fill_null_balances(apps, schema_editor):
+    LeaveBalance = apps.get_model("hr", "LeaveBalance")
+    # NULL -> 0
+    LeaveBalance.objects.filter(relax_leave__isnull=True).update(relax_leave=0)
+    LeaveBalance.objects.filter(maternity_leave__isnull=True).update(maternity_leave=0)
+    LeaveBalance.objects.filter(other_leave__isnull=True).update(other_leave=0)
+    # กันค่าติดลบ (เผื่อเคยเพี้ยน)
+    LeaveBalance.objects.filter(annual_leave__lt=0).update(annual_leave=0)
+    LeaveBalance.objects.filter(sick_leave__lt=0).update(sick_leave=0)
+    LeaveBalance.objects.filter(personal_leave__lt=0).update(personal_leave=0)
+    LeaveBalance.objects.filter(relax_leave__lt=0).update(relax_leave=0)
+    LeaveBalance.objects.filter(maternity_leave__lt=0).update(maternity_leave=0)
+    LeaveBalance.objects.filter(other_leave__lt=0).update(other_leave=0)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        # ปรับให้ตรงกับไฟล์ migration ล่าสุดของแอป hr คุณ
+        ("hr", "0008_leavebalance_maternity_leave_and_more"),
+    ]
+
+    operations = [
+        migrations.RunPython(fill_null_balances, reverse_code=migrations.RunPython.noop),
+    ]
